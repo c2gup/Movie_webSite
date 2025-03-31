eddcb7896a652284685754e0c69fcb8cf22eb617
Anupam Gupta <c2gupt@gmail.com>
2025-03-31 13:48:33 +0530

    new

--- src/components/movie-list/anupam.tsx
+++ src/components/movie-list/anupam.tsx
@@ -4,6 +4,24 @@ function Anupam() {
     return (
         <div>
             Anupam
+            <h1>Movie List</h1>
+            <ul>
+                <li>Movie 1</li>
+                <li>Movie 2</li>
+                <li>Movie 3</li>
+                <li>Movie 4</li>
+                <li>Movie 5</li>
+                <li>Movie 6</li>
+                <li>Movie 7</li>
+                <li>Movie 8</li>
+
+                <li>Movie 9</li>
+                <li>Movie 10</li>
+
+
+
+
+             </ul>   
         </div>
     );
 }
